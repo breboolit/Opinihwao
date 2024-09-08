9d98abf5f8ec6281d71fbcb582acb4cd2d483882
breboolit <breaparecidaferreiradasilva@gmail.com>
2024-09-08 14:21:18 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -2,6 +2,7 @@ let dados = [
     {
         nome: "Jujutsu Kaisen",
         sinopse: "Um jovem estudante descobre que possui uma força sobrenatural e é recrutado para uma escola de feitiçaria com o objetivo de combater maldições.",
+        opiniao : "Jujutsu Kaisen é um mangá que consegue explorar muitas emoções no leitor. Uma delas é a admiração da arte, pois o traço dos personagens é incrivelmente bonito, além de ter uma estética que ao mesmo tempo que é sombria, também consegue ser engraçadinha e fofa. Os personagens, por lidar sempre com maldições e conviverem em situações perigoras diariamente, também conseguem ter seus momentos de pessoas jovens comuns, como sair para comer com os amigos, ir fazer compras, ter uma vida de Fanboy, assim como o Todou, ou até mesmo ser CLT, como o Nanami (piadas à parte). Então, por esses e outros mais motivos, acabamos por nos colocar no lugar dos personagens, e assim, também nos apegamos aos mesmos e sentimos quando um - ou vários deles - morrem. O autor não tem dó de seus personagens!",
         numeroEdicoes : "Em andamento",
         link : "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen",
         tags : "satoro satoru gojo suguru geto yuji itadori nanami kento nobara kugisaki terror expansao de dominio sukuna feiticeiro megumi fushiguro todou maki panda"
@@ -10,6 +11,7 @@ let dados = [
     {
         nome : "Spy x Family",
         sinopse : "Spy x Family é um anime e mangá que acompanha a vida de um espião que precisa criar uma família falsa para completar uma missão.",
+        opiniao : "Spy X Family é um mangá e também anime, que apesar de ter seus momentos de aventura, com as ações de espionagem do Agente Twilight (também conhecido como Loid Forger), é profundamente familiar. Mesmo sendo uma família falsa, os três se comportam e cuidam uns dos outros como se fossem uma família real, e isso apenas mostra que família não se trata apenas parentes de sangue, mas sim vínculos construídos entre as pessoas. Loid tenta se manter sempre profissional, tentando não criar vínculos reais com Anya e Yor, sempre falhando miseravelmente e agindo como um bom pai e marido. A relação de Anya e Yor também é muito preciosa, pois é uma relação de maternidade recente, já que Yor, apesar de ter cuidado do irmão na infância, ainda é difícil para ela agir como mãe da Anya, mas ainda assim é adorável ver a aproximação de ambas.",
         numeroEdicoes : "12 volumes, em andamento",
         link: "https://pt.wikipedia.org/wiki/Spy_x_Family",
         tags : "agente twilight anya yor loid forger familiar comedia"
@@ -18,33 +20,37 @@ let dados = [
     {
         nome : "Demon Slayer",
         sinopse : "Demon Slayer é um anime e mangá que narra a história de Tanjiro Kamado, um jovem que se torna um caçador de demônios para vingar a morte de sua família.",
+        opiniao : "Demon Slayer é muito emocionante e frenético, dado ao fato de que consegue equilibrar sua trama entre lutas alucinantes contra demônios e o desenvolvimento emocional dos personagens, com Tanjiro e sua busca ininterrupta pela cura da irmã, que foi amaldiçoada. A obra também conta com personagens incrivelmente carismáticos, que ganham o coração dos leitores, como o protagonista Tanjiro, Nezuko e Inosuke, um dos mais peculiares por possuir a cabeça de um javali. Por mencionar a peculiaridade de seus personagens, é indispensável os elogios à cartunista, que cria personagens tão originais em uma mesma obra! Outro ponto positivo da obra está na mulher que o escreveu e ilustrou, Koyoharu Gotoge, pois a autora traz muitas personagens femininas e ao contrário do que muitos mangás reproduzem, a autora foi cautelosa com suas personagens, não as usando apenas como belos corpos, fazendo-as de alívio cômico, mas sim deu espaço para essas personagens serem bem desenvolvidas e exploradas, deixando de lado a objetificação das personagens femininas.",
         numeroEdicoes : "3 temporadas",
         link: "https://pt.wikipedia.org/wiki/Demon_Slayer",
-        tags : "tanjiro kamado nezuko inosuke hashibira rengoku mitsuri kanroji vingança"
+        tags : "tanjiro kamado nezuko inosuke hashibira mitsuri kanroji vingança"
     },
 
     {
-        nome : "Attack on Titan",
-        sinopse : "A humanidade vive dentro de muralhas para se proteger de titãs gigantes que devoram humanos.",
-        numeroEdicoes: "4 temporadas",
-        link : "https://pt.wikipedia.org/wiki/Shingeki_no_Kyojin",
-        tags : "eren yeager mikasa ackerman levi ackerman armin arlert reiner braun shingeki no kyojin"
+        nome : "Business Proposal",
+        sinopse : "Em um mundo corporativo onde as propostas de negócios são a chave para o sucesso, Shin Hari, uma jovem trabalhadora, se vê em uma situação inesperada. Para impressionar o CEO da empresa, ela finge ser uma rica herdeira e acaba se envolvendo em um jogo de mentiras e paixão.",
+        opiniao : "Este manhwa e também webtoon, que em 2022 ganhou muito reconhecimento em sua adaptação para K-drama, é muito agradável e de fácil leitura, apesar de ter suas problemáticas ao mostrar um CEO que, ao ver a necessidade de sua funcionária, a faz aceitar uma proposta absurda, ao invés do mesmo ser sincero sobre seus reais sentimentos por ela. O segundo principal, consegue roubar a atenção dos leitores e telespectadores por ser um casal mais autêntico, sem tantos conflitos para iniciarem o relacionamento.",
+        numeroEdicoes: "7 edições",
+        link : "https://pt.wikipedia.org/wiki/Business_Proposal",
+        tags : "hari youngseo ceo drama webtoon"
     },
 
     {
         nome: "One Piece",
         sinopse: "Um jovem pirata em busca do tesouro mais famoso do mundo, o One Piece.",
+        opiniao : "One Piece é um clássico dos mangás e animes, devido ao legado que esta obra continua a construir. Tem uma lore muito bem desenvolvida e trabalhada, e que ainda tem muitas incógnitas à serem descobertas pelo público que o acompanha. Os personagens são desenhados com muita singularidade, mostrando a criatividade do cartunista, ao desenhar seus personagens com traços marcantes, por exemplo o Zoro com sua cicatriz no olho, e o Sanji com o bigode enrolado para cima. É incrível ver o desenvolvimento dos personagens à cada Timeskip, aprimorando suas habilidades e convicções. O único ponto que pode ser um pouco assustador para algumas pessoas é o tamanho deste mangá, que conta com mais de 1000 capítulos! Pessoas que nunca assistiram o anime ou até mesmo leram o mangá observam esse tamanho exacerbado e muitas vezes se interessam pela obra, mas acabam não desejando começar a acompanhá-la. Porém, dado à qualidade do mangá, vale o esforço.",
         numeroEdicoes: "Mais de 1000 capítulos",
         link: "https://pt.wikipedia.org/wiki/One_Piece",
         tags : "luffy zoro nami chopper usopp sanji chapeu de palha marinha navio shanks robin"
     },
     
     {
-        nome: "Naruto",
-        sinopse: "A história de um ninja que busca reconhecimento e sonha em se tornar o Hokage de sua vila.",
-        numeroEdicoes: "72 volumes",
-        link: "https://pt.wikipedia.org/wiki/Naruto",
-        tags : "naruto sasuke kakashi itachi sakura"
+        nome: "True Beauty",
+        sinopse: "Im Joo-kyung, uma adolescente insegura com sua aparência, descobre o poder da maquiagem e se transforma em uma verdadeira beleza. Ela navega entre a vida escolar, as amizades e os romances, aprendendo a lidar com a beleza interior e exterior.",
+        opiniao : "True Beauty tem como pauta assuntos muito sérios que estão presentes no cotidiano da Coreia do Sul, como os inalcançáveis padrões de beleza impostos pela sociedade coreana, bullying e baixa autoestima. Porém, a obra, que possuí um alcance altíssimo em questões de público que o acompanha, que em sua maioria são meninas adolescentes, não se preocupa na forma como retrata o bullying e o auto-aceitação para seus leitores, que vão levar aquilo como verdade. O bullying retratado na trama é muito cruel, e muitas vezes é usado como alívio cômico, o que irrita o leitor (irritou a mim...), e a auto-aceitação da protagonista só acontece quando a mesma está totalmente maquiada e diferente da sua face real, ensinando jovens meninas que elas precisam de maquiagem se quiserem que alguém ache elas bonitas. Do meu ponto de vista, a obra tenta mostrar que é melhor alguém de fora te achar bonita, do que você mesmo, quando na verdade, tem que ser o contrário. Temos que nos achar bonitas, mesmo sem ninguém dizendo se somos ou não.",
+        numeroEdicoes: "Em andamento",
+        link: "https://pt.wikipedia.org/wiki/True_Beauty",
+        tags : "romance jookyung suho seojun drama webtoon manhwa"
     }
 ];
 
